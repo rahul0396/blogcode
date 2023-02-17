92e1e38ddbd49f17cd55ae207f212d84b2e69c19
rahul <rahul.sharm0396@gmail.com>
2023-02-17 15:06:22 +0530

    changes in og

--- src/pages/Contactus.js
+++ src/pages/Contactus.js
@@ -10,21 +10,25 @@ const Contactus = () => {
       
       
       <title>Contact us</title>
+      <meta
+  name="getblogs"
+  content="Check out iPhone 12 XR Pro and iPhone 12 Pro Max. Visit your local store and for expert advice."
+/>
 
                   {/* facebook og tag */}
                   <meta property="og:url"        content="https://getblog2.netlify.app/blog" />
         <meta property="og:type"       content="blog" />
-        <meta property="og:title"      content="Hunting Coder A Place for coders to read and write a blog." />
-        <meta property="og:description"content="Hunting coders a place for all the peoples whose are learning the new things that can read and write a blog." />
-        <meta property="og:image"      content={Image1} />
+        <meta property="og:title"      content="Getblogs :A Place for coders to read and write a blog." />
+        <meta property="og:description"content="Getblogs coders a place for all the peoples whose are learning the new things that can read and write a blog." />
+        <meta property="og:image"      content="https://example.com/images/cool-page.jpg" />
           {/* twitter og tag */}
           <meta name="twitter:card" content="blog" />
-          <meta name="twitter:site" content="@huntingcode" />
+          <meta name="twitter:site" content="@getblogs" />
           <meta name="twitter:creator" content="@rahulsh" />
           <meta property="og:url" content="https://getblog2.netlify.app/blog" />
-          <meta property="og:title" content="blog-huntingcode" />
+          <meta property="og:title" content="blog-getblogs" />
           <meta property="og:description" content="Hunting coders a place for all the peoples whose are learning the new things that can read and write a blog." />
-          <meta property="og:image" content="https://picsum.photos/200" />
+          <meta property="og:image" content="https://example.com/images/cool-page.jpg" />
     </Head>
     <div className={styles.container}>
       <h1>Contact US</h1>
